192865dfd67c0a75ac96cc056f45eaa8b032e1db
giovanyrosales <gioer21@gmail.com>
2023-04-04 13:52:19 -0600

    cambios

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.1.86',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'restaurant_ci',
